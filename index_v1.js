2df6561fe34b3e30a4624fc5f5f85446f510b121
MikackermanDev <122027129+MikackermanDev@users.noreply.github.com>
2023-05-14 19:23:08 +0200

    save

---
+++ index_v1.js
@@ -0,0 +1,9 @@
+let typed = new Typed(document.querySelector(".frappe-auto"), {
+	strings: ["Full-Stack^2000", "Back-End^2000", "Front-End^2000"],
+	typeSpeed: 50,
+	showCursor: false,
+	loop: true,
+	fadeOut: true,
+	fadeOutClass: "typed-fade-out",
+	fadeOutDelay: 1000,
+});
